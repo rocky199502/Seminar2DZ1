3a7e23892e572df44125014bc20ca88368bbb18e
Anastasia <nastys11@gmail.com>
2022-09-03 19:25:43 +0300

    Добавление кода

--- Program.cs
+++ Program.cs
@@ -1,12 +1,20 @@
 ﻿using System;
-
+// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
 namespace Semin2
 {
     class Program
     {
-        static void Main(string[] args)
+        static int NewNumber = 0;
+        static int SecondNumber(int Number)
         {
-            Console.WriteLine("Hello World!");
+            NewNumber = (Number % 100 - Number % 10)/10;
+            return NewNumber;
+        }
+        static void Main(string[] args) {
+        Console.WriteLine("Введите трехзначное число:");
+        int chislo = int.Parse(Console.ReadLine());
+        int result = SecondNumber(chislo);
+        Console.WriteLine($"Вторая цифра числа {chislo} это: {result}");
         }
     }
 }
